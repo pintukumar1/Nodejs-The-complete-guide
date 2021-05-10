c238f1d7670478d48e298c4a58e324608457783b
pintukumar1 <kumarpintu1713122@gmail.com>
2021-05-10 17:41:07 +0530

    main file

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const http = require('http')
-const routes = require('./routes')
+const express = require('express')
 
-console.log(routes.someText)
+const app = express()
 
-const server = http.createServer(routes)
+const server = http.createServer(app)
 
 server.listen(3000, () => {
     console.log('Server is up on port 3000')
